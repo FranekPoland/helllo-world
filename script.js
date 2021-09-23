7baba2f143a443f27f4871f9c315d67d4add1e07
Franek <klocekfranciszek@gmail.com>
2021-09-23 16:14:13 +0200

    First steps

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-console.log('start', document)
-var button = document.querySelectorAll('.button');
-button.textContent = 'lulu';
-console.log('stop', button)
+function getName() {
+    var name = document.querySelector('.name');
+    console.log('name', document);
+    return name;
+}
+
+function send() {
+    test();
+}
+
+function display() {
+    var button = document.querySelector('.button');
+    button.innerHTML = 'lulu';
+    
+    if (button.style.background == 'red none repeat scroll 0% 0%') { 
+        button.style.background = 'yellow'
+    }
+    else {
+        button.style.background = 'red'
+
+    }
+}
+
+function test() {
+    var button = document.getElementsByClassName('.button');
+    console.log('tester', button.style)
+    if (button.style.background == 'red none repeat scroll 0% 0%') { 
+        return console.log('test passed')
+    }
+    console.log('test failed')
+}
