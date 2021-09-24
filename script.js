8b522ccfcabbd6252b2c2eb2af0835132be3fbe3
tymoteuszKlocek <tymoteuszKlocek@users.noreply.github.com>
2021-09-24 11:31:59 +0200

    radio input added (#4)
    Co-authored-by: Franek <klocekfranciszek@gmail.com>

--- script.js
+++ script.js
@@ -1,31 +1,53 @@
-function getName() {
-    var name = document.querySelector('.name');
-    console.log('name', document);
-    return name;
-}
+// function getName() {
+//     var name = document.querySelector('.name');
+//     console.log('name', document);
+//     return name;
+// }
 
-function send() {
-    test();
-}
+// function send() {
+//     test();
+// }
 
-function display() {
-    var button = document.querySelector('.button');
-    button.innerHTML = 'lulu';
-    
-    if (button.style.background == 'red none repeat scroll 0% 0%') { 
-        button.style.background = 'yellow'
-    }
-    else {
-        button.style.background = 'red'
+// function display() {
+//     var button = document.querySelector('.button');
+//     button.innerHTML = 'lulu';
 
-    }
-}
+//     if (button.style.background == 'red none repeat scroll 0% 0%') {
+//         button.style.background = 'yellow'
+//     }
+//     else {
+//         button.style.background = 'red'
 
-function test() {
-    var button = document.getElementsByClassName('.button');
-    console.log('tester', button.style)
-    if (button.style.background == 'red none repeat scroll 0% 0%') { 
-        return console.log('test passed')
-    }
-    console.log('test failed')
-}
+//     }
+// }
+
+// function test() {
+//     var button = document.getElementsByClassName('.button');
+//     console.log('tester', button.style)
+//     if (button.style.background == 'red none repeat scroll 0% 0%') {
+//         return console.log('test passed')
+//     }
+//     console.log('test failed')
+// }
+
+//---------------------------RADIO----------------------------------
+
+var submit = document.querySelector('.submit');
+// TODO add btn Clear
+
+var log = document.querySelector("#girl-answer");
+
+submit.addEventListener("click", function () {
+
+    var radios = document.querySelectorAll("input[type='radio']");
+    var answer = 'You chose: ';
+
+    radios.forEach(function (radio) {
+        if (radio.checked) {
+            answer = answer + radio.value + '. Please take your medicine.';
+        }
+    });
+
+    log.innerText = answer;
+
+}, false);
